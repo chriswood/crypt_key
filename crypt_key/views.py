56584e3e8a1e53f754bb88d93052722efda89392
Chris Wood <chris314159@gmail.com>
2013-02-01 23:13:27 -0600

    Added form to main view and routed post to the same.

--- crypt_key/views.py
+++ crypt_key/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-    
-def main(request, action=None):
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt  
+def main(request):
     '''
-    Process the requests for updating values in the puzzle, and generating
-    new puzzles (coming soon).
-    I hardcoded 9x9, but all code in this program is flexible
-    for any dimension, providing it is a square. So we should be able to
-    handle different puzzle square type games just by writing a new solver.
+        main page to enter a puzzle
     '''
-    context_dict = {}
+    context_dict = {}    
+    if request.method == 'POST':
+        context_dict['rtype'] = 'Is POST'
+    else:
+        context_dict['rtype'] = 'NOT POST'
+
     context_dict['title'] = 'decrypting'
     return render_to_response('main.html',
                               context_dict,
                               context_instance=RequestContext(request))
+
+
